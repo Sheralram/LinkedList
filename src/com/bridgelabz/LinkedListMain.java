1c7945f622cee82827737725ae8b7a47da56abfa
Sheralram <ramkrishna.sheral@gmail.com>
2021-08-25 22:29:38 +0530

    Added:Simple Linked List

---
+++ src/com/bridgelabz/LinkedListMain.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+public class LinkedListMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Linked List");
+        LinkedList list=new LinkedList();
+        list.add(56);
+        list.add(30);
+        list.add(70);
+        list.display();
+
+    }
+}
