9ea7dddc915e964ca6cd012a61b7574a84bab33d
Sheralram <ramkrishna.sheral@gmail.com>
2021-08-25 23:48:14 +0530

    POP: First element is deleted

--- src/com/bridgelabz/LinkedListMain.java
+++ src/com/bridgelabz/LinkedListMain.java
@@ -3,10 +3,16 @@ package com.bridgelabz;
 public class LinkedListMain {
     public static void main(String[] args) {
         System.out.println("Welcome To Linked List");
+        System.out.println("Nodes of singly linked list:");
         LinkedList list=new LinkedList();
         list.add(56);
         list.add(70);
+        list.display();
+        System.out.println("30 is Inserting between 56 and 70");
         list.insert(list.head, 30);
         list.display();
+        list.pop();
+        list.display();
+        
     }
 }
